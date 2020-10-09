cef393296fa06758d650b3e825fc75f9579728d1
danielomunoz <danielmunozamaya1@gmail.com>
2020-10-09 18:28:23 +0200

    Diferenciamos entre un proyecto sql y no-sql

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # easyback.io
 
+* Versión de NodeJS: 12.18.4
+* Versión de MySQL: 8.0.12
+
 ## Primer commit: Crear package.json
-Funciona perfectamente, pero hay que tener cuidado porque al posicionarnos en el folder del back creado, no podemos volver a ejecutar el script generate.js, pues este implica borrar el folder del back creado para regenerarlo, y dará error. Por lo demás, funciona genial al hacer ````npm install```
+Funciona perfectamente, pero hay que tener cuidado porque al posicionarnos en el folder del back creado, no podemos volver a ejecutar el script generate.js, pues este implica borrar el folder del back creado para regenerarlo, y dará error. Por lo demás, funciona genial al hacer ```npm install```
+
--- generator.js
+++ generator.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const utils = require('./utils/utils.js');
 
-const createPackageJsonAndServer = (template) => {
+const createMongoPackageJsonAndServer = (template) => {
 	const package_json_path = path.join(__dirname, 'backends', 'backend', 'package.json');
 
 	let package_json = {};
@@ -20,13 +20,36 @@ const createPackageJsonAndServer = (template) => {
 	fs.writeFileSync(package_json_path, JSON.stringify(package_json, null, 2));
 }
 
+const createSqlPackageJsonAndServer = (template) => {
+	const package_json_path = path.join(__dirname, 'backends', 'backend', 'package.json');
+
+	let package_json = {};
+
+	package_json.name = template.package_json["name"] || "default";
+	package_json.version = template.package_json["version"] || "v1.0.0";
+	package_json.description = template.package_json["description"] || "default";
+	package_json.main = template.package_json["main"] || "server.js";
+	package_json.scripts = template.package_json["scripts"] || { "test": "echo \"Error: no test specified\" && exit 1" };
+	package_json.author = template.package_json["author"] || "default";
+	package_json.license = template.package_json["license"] || "ISC";
+	package_json.dependencies = template.package_json["dependencies"] || { "body-parser": "^1.19.0", "express": "^4.17.1", "express-validator": "^6.6.1", "jsonwebtoken": "^8.5.1", "mysql2": "^2.2.2", "node-fetch": "^2.6.1", "sequelize": "^6.3.5" };
+
+	fs.writeFileSync(package_json_path, JSON.stringify(package_json, null, 2));
+}
+
 const createBackend = async () => {
 
 	const template = utils.readTemplate();
 
 	await utils.clearBackendFolder();
 
-	createPackageJsonAndServer(template);
+	if(template.global.db_type == "sql"){
+		createSqlPackageJsonAndServer(template);
+	} else {
+		createMongoPackageJsonAndServer(template);
+	}
+	
+	
 
 }
 
